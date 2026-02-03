e026e1e0dc38da59ea23fe25062949a6c10e3467
Olga Berezhna <57196165+oberezhnay@users.noreply.github.com>
2026-02-03 16:31:34 +0200

    add files

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,31 @@
 'use strict';
+let slickInitialized = false;
+
+function initSlick() {
+  const width = window.innerWidth;
+
+  if (width < 1280) {
+    if (!slickInitialized) {
+      $('.slider').slick({
+        dots: true,
+        arrows: false,
+        infinite: false,
+        slidesToShow: width < 768 ? 1 : 2,
+        adaptiveHeight: false,
+        centerMode: false,
+        centerPadding: '60px'
+      });
+      slickInitialized = true;
+    }
+  } else {
+    if (slickInitialized) {
+      $('.slider').slick('unslick');
+      slickInitialized = false;
+    }
+  }
+}
+
+window.addEventListener('DOMContentLoaded', function () {
+    initSlick();
+    $(window).on('resize', initSlick);
+  });
